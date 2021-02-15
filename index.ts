510a9476f9d9ecf9f03b9870e8c0303d3c364393
Felipe Schmitt <492108+fsschmitt@users.noreply.github.com>
2021-02-15 19:11:07 +0000

    feat(standalone): run delivery without webserver

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
-const express = require('express');
-const routes = require('./src/routes');
-const app = express();
-const port = process.env.PORT || 80;
+import { ResponseType } from "./src/types/responseTypes";
+import * as stockService from './src/datasources/yahoo';
+import { buildResponse } from "./src/utils/responseBuilder";
 
-routes(app);
+const args = process.argv.slice(2);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+if(args.length === 0) {
+    const express = require('express');
+    const routes = require('./src/routes');
+    const app = express();
+    const port = process.env.PORT || 80;
+    routes(app);
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
+}
+
+
+const results = args.map(ticker => stockService.fetchData(ticker));
+Promise.all(results).then(stocks => {
+    console.log(buildResponse(stocks, ResponseType.text));
 });
